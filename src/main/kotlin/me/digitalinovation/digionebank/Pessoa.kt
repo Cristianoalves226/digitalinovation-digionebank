72111aeafd97b5f7b22ec71b44039ac57ad45d6e
Cristianoalves226 <c.alvesdecampos@gmail.com>
2022-02-04 17:05:43 -0400

    Modificador de acesso em klotin

--- src/main/kotlin/me/digitalinovation/digionebank/Pessoa.kt
+++ src/main/kotlin/me/digitalinovation/digionebank/Pessoa.kt
@@ -1,5 +1,16 @@
 package me.digitalinovation.digionebank
 
 class Pessoa {
+    var nome: String = "Cristiano"
+    var cpf: String = "123.123.123-12"
 
+
+}
+
+fun main() {
+    val cristiano = Pessoa()
+    cristiano.cpf = "1234"
+    println(cristiano)
+    println(cristiano.nome)
+    println(cristiano.cpf)
 }
