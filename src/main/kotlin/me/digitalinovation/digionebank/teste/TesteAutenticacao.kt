077aa2d008f8450e3d96d2b7eb767f0cf2d7f04e
Cristianoalves226 <c.alvesdecampos@gmail.com>
2022-02-05 13:18:24 -0400

    Conceito de Polimorfismo kotlin

---
+++ src/main/kotlin/me/digitalinovation/digionebank/teste/TesteAutenticacao.kt
@@ -0,0 +1,7 @@
+package me.digitalinovation.digionebank.teste
+
+import me.digitalinovation.digionebank.Logavel
+
+class TesteAutenticacao {
+    fun autentica(logavel: Logavel) = println(logavel.login())
+}
--- src/main/kotlin/me/digitalinovation/digionebank/teste/TesteFuncionario.kt
+++ src/main/kotlin/me/digitalinovation/digionebank/teste/TesteFuncionario.kt
@@ -1,10 +1,11 @@
 package me.digitalinovation.digionebank.teste
 
-import me.digitalinovation.digionebank.Analista
-import me.digitalinovation.digionebank.Funcionario
+import me.digitalinovation.digionebank.Gerente
 import me.digitalinovation.digionebank.ImprimeRelatorioFuncionario
 
 fun main() {
-    val joao = Analista("Joao", "123", 1200.0)
+    val joao = Gerente("Joao", "123", 1200.0,"senha123")
     ImprimeRelatorioFuncionario.imprime(joao)
+
+    TesteAutenticacao().autentica(joao)
 }
